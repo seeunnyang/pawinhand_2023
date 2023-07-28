95742b3d336e55cdd437ce3b5d0f04c4cc03fc67
seeun <choise0804@naver.com>
2023-07-28 17:36:51 +0900

    swiper_slide

--- script/script.js
+++ script/script.js
@@ -32,3 +32,41 @@ close.addEventListener('click',function(){
     //3-2. 모바일 메뉴 숨기기
     m_nav_open.style.display = 'none';
 });
+
+// main-swiper-slide
+// 문법=> const 변수명 작성 = new Swiper('적용대상');
+// 문법=> const 변수명 작성 = new Swiper('적용대상',{속성:값, 속성:값});
+const pawin_slide = new Swiper('#pawin_slide',{
+    autoplay:{
+        delay:1000, //슬라이드 간격(밀리초) 기본3000(3초)
+        disableOnInteraction:false, //버튼 클릭 후 자동재생유지
+    }, //자동재생
+    loop:true, //반복설정 마지막슬라이드→첫번째슬라이드 자연스러운 변경
+    effect:'fade', //제자리 자연스러운 변경 슬라이드 효과
+    // direction:'vertical' //수직으로 넘어가기
+    navigation: { //슬라이드 이전, 다음 버튼 (페이지 복붙) 
+        //body 안에 있는 모든 swiper-next,prev를 인식하기 때문에
+        //개별인식가능한 부모이름을 반드시 앞에 먼저 작성한다.
+        nextEl: '#pawin_slide .swiper-button-next',
+        prevEl: '#pawin_slide .swiper-button-prev',
+    },
+});
+
+
+
+
+//------------------------------------------------------------------------------------
+//1160*300 slide*3 swiper 생성
+// 문법=> const 변수명 작성 = new Swiper('적용대상',{속성:값, 속성:값});
+const slide_2 = new Swiper('#slide_2',{
+    autoplay:{
+        delay:1000, //슬라이드 간격(밀리초) 기본3000(3초)
+        disableOnInteraction:false, //버튼 클릭 후 자동재생유지
+    }, //자동재생
+    loop:true,
+    effect:'cube',
+    navigation: { /* 슬라이드 이전, 다음 버튼 (페이지 복붙, 부모 이름 붙여주기) */ 
+        nextEl: '#slide_2 .swiper-button-next',
+        prevEl: '#slide_2 .swiper-button-prev',
+    },
+})
